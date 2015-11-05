90bc0901f31777a58763d4c80fb4d22af9045ac4
ZodiacFireworks <martin.vuelta@gmail.com>
2015-11-05 12:47:43 -0500

    added distance sensing

---
+++ DistanceSensingSR04/DistanceSensingSR04.ino
@@ -0,0 +1,32 @@
+// ---------------------------------------------------------------------------
+// Example NewPing library sketch that does a ping about 20 times per second.
+// ---------------------------------------------------------------------------
+
+#include <NewPing.h>
+
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+
+int DELAY = 100;
+
+void setup() {
+  Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    DELAY = Serial.parseInt();
+    
+    Serial.print("M:");
+    Serial.println(DELAY);
+  }
+
+  Serial.print("S:0|D:");
+  Serial.print(sonar.ping_cm());
+  Serial.print("|0:");
+  Serial.println(0);
+  delay(DELAY);
+}
